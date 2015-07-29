1e6e14bbd5f0a15fe00c58de53d71011ed5b8404
Elkin Arriero <e.arriero.g@gmail.com>
2015-07-29 10:42:34 -0500

    In order to recreate dummy data I added gem faker, and with it I created Customers and its related fields, also on table accounts created an employees's index. Finally, I learnt about scopes and created premier, newest and starts_with scopes in customer.rb model.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,9 @@
 class Customer < Account
 	has_many :time_entries
+
+	scope :premier, -> { where("employees > ?", 80)}
+	scope :newest, -> { order("created_at DESC") } 
+	scope :starts_with, -> (letter) { where("name LIKE ?", letter + "%") }
 end
 
 #@customer.time_entries #Solamente retorna TimeEntries
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728191857) do
+ActiveRecord::Schema.define(version: 20150729141025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,13 @@ ActiveRecord::Schema.define(version: 20150728191857) do
     t.string   "about"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "city"
+    t.integer  "zipcode"
+    t.string   "state"
+    t.integer  "employees"
   end
+
+  add_index "accounts", ["employees"], name: "index_accounts_on_employees", using: :btree
 
   create_table "employees_projects", force: :cascade do |t|
     t.integer  "employee_id"
