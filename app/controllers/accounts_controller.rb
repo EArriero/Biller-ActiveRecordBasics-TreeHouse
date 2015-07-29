c7b56403f89a45cba865b20cf08bf07c9021a52e
Elkin Arriero <e.arriero.g@gmail.com>
2015-07-28 19:51:33 -0500

    Added employees and time_entries controllers.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,7 @@
 class AccountsController < ApplicationController
 	def index
-		@accounts = Account.all
+		# Comment. En el metodo defino Customer.all en vez de Account.all porque en la vista de Accounts deseo mostrar solo las cuentas de los cleintes, y no a lo clientes y empeados.
+		@accounts = Customer.all
 	end
 
 	def show
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,8 @@
 class Account < ActiveRecord::Base
 	has_many :account_entries
+
+	validates :name, presence: true,
+					 length: { in: 1..70,
+					   message: "El nombre debe tener entre 1 y 70 caracteres" },
+					 uniqueness: true
 end
